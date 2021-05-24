363382912e67e46b3c9e0afd2e68aee5e8e26cac
Gerald Versluis <gerald@verslu.is>
2021-05-24 12:12:30 +0200

    Implement Android push notifications

---
+++ XFFCMPushNotificationsSample.Android/Application.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Plugin.FirebasePushNotification;
+
+namespace XFFCMPushNotificationsSample.Droid
+{
+    [Application]
+    public class MainApplication : Application
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer) : base(handle, transer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            //Set the default notification channel for your app when running Android Oreo
+            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
+            {
+                //Change for your default notification channel id here
+                FirebasePushNotificationManager.DefaultNotificationChannelId = "FirebasePushNotificationChannel";
+
+                //Change for your default notification channel name here
+                FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
+
+                FirebasePushNotificationManager.DefaultNotificationChannelImportance = NotificationImportance.Max;
+            }
+
+            //If debug you should reset the token each time.
+#if DEBUG
+            FirebasePushNotificationManager.Initialize(this, true);
+#else
+            FirebasePushNotificationManager.Initialize(this, false);
+#endif
+
+            //Handle notification when app is closed here
+            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            {
+
+
+            };
+        }
+    }
+}
---
+++ XFFCMPushNotificationsSample/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Plugin.FirebasePushNotification;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFFCMPushNotificationsSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            CrossFirebasePushNotification.Current.OnTokenRefresh += Current_OnTokenRefresh;
+        }
+
+        private void Current_OnTokenRefresh(object source, FirebasePushNotificationTokenEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"Token: {e.Token}");
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
